da75c4b157238c4e6e50e77f15e399449b89020d
aha5811 <aha5811@users.noreply.github.com>
2023-12-07 12:25:10 +0100

    day 7

--- aha-aoc2023/src/aha/aoc2023/day07/Part1.java
+++ aha-aoc2023/src/aha/aoc2023/day07/Part1.java
@@ -2,29 +2,142 @@ package aha.aoc2023.day07;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+import aha.aoc2023.Utils;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class Part1 {
-	
-	static String dir = "day07/";
-	
-	long res = 0;
 
+	static String dir = "day07/";
+
+	long res = 0;
+	
 	public Part1() {
 	}
-	
+
 	Part1 compute(final String file) {
+		final List<HB> hbs =
+				Utils.streamLines(dir + file)
+				.map(line -> toHB(line))
+				.collect(Collectors.toList());
+		
+		hbs.sort(new Comparator<HB>() {
+			@Override
+			public int compare(final HB hb1, final HB hb2) {
+				if (hb1.rank == hb2.rank) {
+					for (int i = 0; i < hb1.hand.size(); i++) {
+						final int ret = Integer.compare(hb1.hand.get(i), hb2.hand.get(i));
+						if (ret != 0)
+							return ret;
+					}
+					return 0;
+				}
+				return Integer.compare(hb1.rank, hb2.rank);
+			}
+		});
+
+		final AtomicInteger rank = new AtomicInteger();
+		this.res =
+				hbs.stream()
+				.mapToLong(hb -> rank.incrementAndGet() * hb.bid)
+				.sum();
+
 		return this;
 	}
+	
+	/**
+	 * hand + bid
+	 */
+	static class HB {
+		String handS; // for toString
+		List<Integer> hand;
+		int bid;
+		int rank;
+		
+		@Override
+		public String toString() {
+			return this.handS + " (" + this.rank + ") / " + this.bid;
+		}
+	}
+	
+	private HB toHB(final String s) {
+		final String[] cb = s.split(" ");
 
-	@Test
-	public void aTest() {
-		assertEquals(0, new Part1().compute("test.txt").res);
+		final HB hb = new HB();
+		hb.handS = cb[0];
+		hb.bid = Integer.parseInt(cb[1]);
+		hb.hand = toIA(hb.handS, getCh2Val());
+		hb.rank = rank(hb);
+		
+		return hb;
+	}
+	
+	Map<Character, Integer> getCh2Val() { return ch2v; }
+	private final static Map<Character, Integer> ch2v = Map.of('T', 10, 'J', 11, 'Q', 12, 'K', 13, 'A', 14);
+
+	private static List<Integer> toIA(final String s, final Map<Character, Integer> ch2val) {
+		return s.chars().mapToObj(c -> (char) c)
+				.map(c -> Character.isDigit(c) ? Character.getNumericValue(c) : ch2val.get(c))
+				.collect(Collectors.toCollection(ArrayList::new)); // ArrayList for fast get
+	}
+
+	int rank(final HB hb) {
+		final Map<Integer, Integer> card2cnt = cntCards(hb.hand); // card value -> cnt
+		
+		final Collection<Integer> cnts = card2cnt.values();
+		cnts.removeIf(cnt -> cnt == 1);
+		
+		return cnts2rank(cnts);
+	}
+	
+	static Map<Integer, Integer> cntCards(final List<Integer> cards) {
+		final Map<Integer, Integer> card2cnt = new HashMap<>();
+		for (final Integer c : cards)
+			card2cnt.put(c, 1 + (card2cnt.containsKey(c) ? card2cnt.get(c) : 0));
+		return card2cnt;
+	}
+	
+	static int R_FIVE = 6;
+	static int R_FOUR = 5;
+	static int R_FULL_HOUSE = 4;
+	static int R_THREE = 3;
+	static int R_TWO_PAIR = 2;
+	static int R_PAIR = 1;
+	static int R_HIGH = 0;
+
+	int cnts2rank(final Collection<Integer> cnts) {
+		if (cnts.contains(5))
+			return R_FIVE;
+		else if (cnts.contains(4))
+			return R_FOUR;
+		else if (cnts.contains(3))
+			return cnts.contains(2) ? R_FULL_HOUSE : R_THREE;
+		else if (cnts.contains(2))
+			return cnts.size() == 1 ? R_PAIR : R_TWO_PAIR;
+		else
+			return R_HIGH;
 	}
 	
 	@Test
-	public void main() {
-		// assertEquals(0, new Part1().compute("input.txt").res);
+	public void aTest() {
+		assertEquals(6440, new Part1().compute("test.txt").res);
 	}
 
+	@Test
+	public void main() {
+		assertEquals(249638405, new Part1().compute("input.txt").res);
+	}
+	
 }
--- aha-aoc2023/src/aha/aoc2023/day07/Part2.java
+++ aha-aoc2023/src/aha/aoc2023/day07/Part2.java
@@ -2,16 +2,49 @@ package aha.aoc2023.day07;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 public class Part2 extends Part1 {
 	
 	@Override
+	Map<Character, Integer> getCh2Val() { return ch2v; }
+	private final static Map<Character, Integer> ch2v = Map.of('T', 10, 'J', 1, 'Q', 11, 'K', 12, 'A', 13);
+
+	@Override
+	int rank(final HB hb) {
+		
+		final Map<Integer, Integer> card2cnt = cntCards(hb.hand);
+
+		// get/remove jokers
+		final Integer jVal = getCh2Val().get('J');
+		final int jCnt = card2cnt.containsKey(jVal) ? card2cnt.remove(jVal) : 0;
+		
+		final List<Integer> cnts = new LinkedList<>(card2cnt.values());
+		
+		if (jCnt > 0) {
+			// add jokers to highest group
+			Collections.sort(cnts);
+			Collections.reverse(cnts);
+			cnts.add(jCnt + (cnts.isEmpty() ? 0 : cnts.remove(0))); // JJJJJ
+		}
+
+		cnts.removeIf(cnt -> cnt == 1);
+
+		return cnts2rank(cnts);
+
+	}
+	
+	@Override
 	public void aTest() {
-		assertEquals(0, new Part2().compute("test.txt").res);
+		assertEquals(5905, new Part2().compute("test.txt").res);
 	}
 
 	@Override
 	public void main() {
-		// assertEquals(0, new Part2().compute("input.txt").res);
+		assertEquals(249776650, new Part2().compute("input.txt").res);
 	}
 
 }
