cf52fe26324592d61483ecb2810808e922afee45
aha5811 <aha5811@users.noreply.github.com>
2023-12-07 17:25:13 +0100

    day 8 setup

--- aha-aoc2023/src/aha/aoc2023/TestAll.java
+++ aha-aoc2023/src/aha/aoc2023/TestAll.java
@@ -12,7 +12,7 @@ import org.junit.platform.suite.api.Suite;
 	aha.aoc2023.day05.Part1.class, aha.aoc2023.day05.Part2.class,
 	aha.aoc2023.day06.Part1.class, aha.aoc2023.day06.Part2.class,
 	aha.aoc2023.day07.Part1.class, aha.aoc2023.day07.Part2.class,
-//	aha.aoc2023.day08.Part1.class, aha.aoc2023.day08.Part2.class,
+	aha.aoc2023.day08.Part1.class, aha.aoc2023.day08.Part2.class,
 //	aha.aoc2023.day09.Part1.class, aha.aoc2023.day09.Part2.class,
 //	aha.aoc2023.day10.Part1.class, aha.aoc2023.day10.Part2.class,
 //	aha.aoc2023.day11.Part1.class, aha.aoc2023.day11.Part2.class,
