6a276914388d7fc7d83a95f64c3656e2bb90edcc
aha5811 <aha5811@users.noreply.github.com>
2023-12-08 12:22:37 +0100

    day 8

--- aha-aoc2023/src/aha/aoc2023/day08/Part1.java
+++ aha-aoc2023/src/aha/aoc2023/day08/Part1.java
@@ -2,32 +2,74 @@ package aha.aoc2023.day08;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
+import aha.aoc2023.Utils;
+
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class Part1 {
-
-	static String dir = "day08/";
-
-	long res;
 	
+	static String dir = "day08/";
+	
+	int[] instructions;
+	Map<String, String[]> n2rl;
+	
+	long res;
+
 	public Part1() {
 	}
-
+	
 	Part1 compute(final String file) {
+		parseStructures(file);
+
+		this.res = countSteps();
+
 		return this;
+	}
+	
+	private void parseStructures(final String file) {
+		final List<String> lines = Utils.readLines(dir + file);
+
+		this.instructions = lines.remove(0).chars().map(c -> (char) c).map(c -> c == 'L' ? 0 : 1).toArray();
+		
+		lines.remove(0);
+
+		this.n2rl = new HashMap<>();
+		for (final String line : lines) {
+			// AAA = (BBB, CCC)
+			final String[] nrl = line.replaceAll("[\\s+\\(\\)]", "").split("=");
+			this.n2rl.put(nrl[0], nrl[1].split(","));
+		}
+	}
+	
+	long countSteps() {
+		String node = "AAA";
+		int step = 0;
+		while (!"ZZZ".equals(node))
+			node = next(step++, node);
+		return step;
+	}
+
+	String next(final int step, final String node) {
+		final int instruction = this.instructions[step % this.instructions.length];
+		return this.n2rl.get(node)[instruction];
 	}
 	
 	@Test
 	public void aTest() {
-		assertEquals(0, new Part1().compute("test.txt").res);
-	}
-
-	@Test
-	public void main() {
-		// assertEquals(0, new Part1().compute("input.txt").res);
+		assertEquals(2, new Part1().compute("test1_1.txt").res);
+		assertEquals(6, new Part1().compute("test1_2.txt").res);
 	}
 	
+	@Test
+	public void main() {
+		assertEquals(17263, new Part1().compute("input.txt").res);
+	}
+
 }
--- aha-aoc2023/src/aha/aoc2023/day08/Part2.java
+++ aha-aoc2023/src/aha/aoc2023/day08/Part2.java
@@ -2,16 +2,95 @@ package aha.aoc2023.day08;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Part2 extends Part1 {
+	
+	private boolean naive;
+	
+	Part2 withNaive() {
+		this.naive = true;
+		return this;
+	}
+	
+	@Override
+	long countSteps() {
+		final List<String> startNodes =
+				this.n2rl.keySet().stream()
+				.filter(node -> node.endsWith("A"))
+				.collect(Collectors.toList());
+		
+		if (this.naive)
+			return countStepsNaive(startNodes);
+		else
+			return countStepsFast(startNodes);
+	}
+	
+	private int countStepsNaive(List<String> nodes) {
+		int step = 0;
+		while (nodes.stream().filter(node -> !node.endsWith("Z")).count() > 0) {
+			final int instruction = this.instructions[step++ % this.instructions.length];
+			nodes =
+					nodes.stream()
+					.map(node -> this.n2rl.get(node)[instruction])
+					.collect(Collectors.toList());
+		}
+		return step;
+	}
+
+	// may not produce correct results for arbitrary data
+	
+	private long countStepsFast(final List<String> startNodes) {
+		return
+				startNodes.stream()
+				.mapToLong(node -> getFirstStepOnZ(node))
+				.reduce(1l, (res, l) -> lcm(res, l));
+	}
+
+	private long getFirstStepOnZ(String node) {
+		
+		final List<Integer> stepsOnZ = new LinkedList<>();
+		
+		final List<String> nodesAtRoundStart = new LinkedList<>();
+		
+		int step = 0;
+
+		while (true) {
+			if (step % this.instructions.length == 0) { // one round
+				if (nodesAtRoundStart.contains(node)) // cycle
+					break;
+				nodesAtRoundStart.add(node);
+			}
+
+			node = next(step++, node);
+			
+			if (node.endsWith("Z"))
+				stepsOnZ.add(step);
+		}
+		
+		return stepsOnZ.get(0);
+		
+	}
+	
+	private static long lcm(final long a, final long b) {
+		return a * b / gcd(a, b);
+	}
+
+	private static long gcd(final long a, final long b) {
+		return b == 0 ? a : gcd(b, a % b);
+	}
 	
 	@Override
 	public void aTest() {
-		assertEquals(0, new Part2().compute("test.txt").res);
+		assertEquals(6, new Part2().withNaive().compute("test2.txt").res);
+		assertEquals(6, new Part2().compute("test2.txt").res);
 	}
 
 	@Override
 	public void main() {
-		// assertEquals(0, new Part2().compute("input.txt").res);
+		assertEquals(14631604759649l, new Part2().compute("input.txt").res);
 	}
 
 }
