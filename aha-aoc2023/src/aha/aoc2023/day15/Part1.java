7944784d32bd9f824ee7f6fdf97b0289c7fb671f
aha5811 <aha5811@users.noreply.github.com>
2023-12-15 10:32:45 +0100

    day 15 long -> int + comments

--- aha-aoc2023/src/aha/aoc2023/day15/Part1.java
+++ aha-aoc2023/src/aha/aoc2023/day15/Part1.java
@@ -2,46 +2,46 @@ package aha.aoc2023.day15;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 import aha.aoc2023.Part;
 import aha.aoc2023.Utils;
 
 public class Part1 extends Part {
-	
-	static String dir = "day15/";
 
+	static String dir = "day15/";
+	
 	public Part1() {
 	}
-	
+
 	@Override
 	public Part compute(final String file) {
-
-		this.res = getStrings(file).mapToLong(s -> hash(s)).sum();
-
+		
+		this.res = getStrings(file).mapToInt(s -> hash(s)).sum();
+		
 		return this;
 	}
-
+	
 	final Stream<String> getStrings(final String file) {
 		final String line = Utils.readLines(dir + file).get(0);
 		return Stream.of(line.split(","));
 	}
-
-	final long hash(final String s) {
-		final AtomicLong al = new AtomicLong();
+	
+	final int hash(final String s) {
+		final AtomicInteger al = new AtomicInteger();
 		s.chars().forEach(i -> al.set((al.get() + i) * 17 % 256));
 		return al.get();
 	}
-	
+
 	@Override
 	public void aTest() {
 		assertEquals(1320, new Part1().compute("test.txt").res);
 	}
-	
+
 	@Override
 	public void main() {
 		assertEquals(498538, new Part1().compute("input.txt").res);
 	}
-
+	
 }
--- aha-aoc2023/src/aha/aoc2023/day15/Part2.java
+++ aha-aoc2023/src/aha/aoc2023/day15/Part2.java
@@ -11,60 +11,62 @@ import java.util.regex.Pattern;
 import aha.aoc2023.Part;
 
 public class Part2 extends Part1 {
-
+	
 	@Override
 	public Part compute(final String file) {
-
+		
+		// box number (hash) -> label -> focal length
 		final Map<Integer, Map<String, Integer>> h2l2fl = new HashMap<>();
-
+		
 		final Pattern p = Pattern.compile("([a-z]+)([=\\-])(\\d+)?");
-
+		
 		getStrings(file).forEach(s -> {
 			final Matcher m = p.matcher(s);
 			m.matches();
 			final String label = m.group(1);
-			final int hash = (int) hash(label);
+			final int hash = hash(label);
 			if (m.group(2).equals("="))
 				add(h2l2fl, hash, label, Integer.parseInt(m.group(3)));
 			else
 				remove(h2l2fl, hash, label);
 		});
-
+		
 		for (final int h : h2l2fl.keySet()) {
 			int s = 0;
 			for (final String l : h2l2fl.get(h).keySet())
+				// box number + 1 * slot * focal length
 				this.res += (h + 1) * ++s * h2l2fl.get(h).get(l);
 		}
-
+		
 		return this;
 	}
-	
+
 	private void remove(final Map<Integer, Map<String, Integer>> m,
 			final int hash, final String label)
 	{
-		if (!m.containsKey(hash))
-			return;
-		m.get(hash).remove(label);
-		if (m.get(hash).isEmpty())
-			m.remove(hash);
+		if (m.containsKey(hash)) {
+			m.get(hash).remove(label);
+			if (m.get(hash).isEmpty())
+				m.remove(hash);
+		}
 	}
-
+	
 	private void add(final Map<Integer, Map<String, Integer>> m,
 			final int hash, final String label, final Integer fl)
 	{
 		if (!m.containsKey(hash))
-			m.put(hash, new LinkedHashMap<>()); // insertion order
+			m.put(hash, new LinkedHashMap<>()); // preserve insertion order
 		m.get(hash).put(label, fl);
 	}
-
+	
 	@Override
 	public void aTest() {
 		assertEquals(145, new Part2().compute("test.txt").res);
 	}
-	
+
 	@Override
 	public void main() {
 		assertEquals(286278, new Part2().compute("input.txt").res);
 	}
-	
+
 }
