40006c78cefc1b973b1e28fae780840def266cda
aha5811 <aha5811@users.noreply.github.com>
2023-12-19 11:47:45 +0100

    day 18 complete

--- aha-aoc2023/src/aha/aoc2023/day18/Part2.java
+++ aha-aoc2023/src/aha/aoc2023/day18/Part2.java
@@ -12,103 +12,156 @@ import aha.aoc2023.Part;
 
 public class Part2 extends Part1 {
 
+	private static List<String> RIGHT_TURNS = Arrays.asList("UR", "RD", "DL", "LU");
+	private static List<String> LEFT_TURNS = Arrays.asList("UL", "LD", "DR", "RU");
+	
 	private static List<String> RIGHT = Arrays.asList("URD", "RDL", "DLU", "LUR");
 	private static List<String> LEFT = Arrays.asList("DRU", "LDR", "ULD", "RUL");
 	
-	private List<String> spin = RIGHT;
-
-	private Part2 setLeft() {
-		this.spin = LEFT;
-		return this;
-	}
+	private static List<String> OPPOS = Arrays.asList("UD", "DU", "LR", "RL");
 
 	@Override
 	public Part compute(final String file) {
-
 		final List<I> is = new ArrayList<>(readInstructions(file));
-		
-		while (is.size() > 4) {
 
-			pp(is);
-
-			long minA = Long.MAX_VALUE;
-			int minP = -1;
-			
-			for (int p = 1; p < is.size() - 1; p++) {
-				final I i1 = is.get(p - 1), i2 = is.get(p), i3 = is.get(p + 1);
-				final String ds = "" + i1.d + i2.d + i3.d;
-
-				if (this.spin.contains(ds)) {
-					
-					// compute cut rect
-					final long a = Math.min(i1.n, i3.n) * (i2.n + 1);
-
-					if (a < minA) {
-						minA = a;
-						minP = p;
-					}
-				}
+		// find spin
+		List<String> spin = null;
+		{
+			int rightTurns = 0;
+			I last = is.get(0);
+			for (final I i : is.subList(1, is.size())) {
+				final String s = "" + last.d + i.d;
+				if (RIGHT_TURNS.contains(s))
+					rightTurns++;
+				else if (LEFT_TURNS.contains(s))
+					rightTurns--;
+				else
+					System.out.println(s); // no out -> well formed input (no DD or UU or LL or RR)
+				last = i;
 			}
-
-			if (minP != -1) {
-				final I i1 = is.get(minP - 1), i2 = is.get(minP), i3 = is.get(minP + 1);
-				
-				// cut off rectangle
-				
-				final long minN = Math.min(i1.n, i3.n);
-				this.res += minN * (i2.n + 1);
-				
-				i1.n -= minN;
-				i3.n -= minN;
-
-				// remove the now zero length instruction
-
-				if (i1.n == 0)
-					is.remove(i1);
-				if (i3.n == 0)
-					is.remove(i3);
-			}
-
-			// simplify
-			for (int p = 0; p < is.size(); p++) {
-				final I i1 = is.get(p - 1 == -1 ? is.size() - 1 : p - 1), i2 = is.get(p);
-				if (i1.d == i2.d) {
-					i1.n += i2.n;
-					is.remove(i2);
-					p--;
-				} else {
-					final String s = "" + i1.d + i2.d;
-					if (s.equals("RL") || s.equals("LR") || s.equals("UD") || s.equals("DU")) {
-						if (i1.n > i2.n) {
-							i1.n -= i2.n;
-							this.res += i2.n;
-							i2.n -= i2.n;
-						} else {
-							i2.n -= i1.n;
-							this.res += i1.n;
-							i1.n -= i1.n;
-						}
-						if (i1.n == 0) {
-							is.remove(i1);
-							p--;
-						}
-						if (i2.n == 0) {
-							is.remove(i2);
-							p--;
-						}
-					}
-				}
-			}
-			
+			spin = rightTurns > 0 ? RIGHT : LEFT;
 		}
 		
 		pp(is);
+		
+		while (is.size() > 4) { // rect can't be reduced without lots of bounds checking
 
-		this.res += (long) (is.get(0).n + 1) * (is.get(1).n + 1);
+			// find middle instruction of 3/4 loop with minimum area (to prevent
+			// self-overlapping)
+
+			int pMin = -1;
+			{
+				long aMin = Long.MAX_VALUE;
+				for (int p = 0; p < is.size(); p++) {
+					final IAround iii = getIAround(is, p);
+					final String ds = iii.toDs();
+					if (spin.contains(ds)) {
+						
+						// rect area
+						final long a = Math.min(iii.i1.n, iii.i3.n) * (iii.i2.n + 1);
+						
+						if (a < aMin) {
+							aMin = a;
+							pMin = p;
+						}
+					}
+				}
+			}
+
+			// cut off rectangle
+			
+			{
+				final IAround iii = getIAround(is, pMin);
+				
+				final long minN = Math.min(iii.i1.n, iii.i3.n);
+				this.res += minN * (iii.i2.n + 1); // +1 for border
+				iii.i1.n -= minN;
+				iii.i3.n -= minN;
+
+				// remove the now zero length instruction(s)
+
+				if (iii.i1.n == 0)
+					is.remove(iii.i1);
+				if (iii.i3.n == 0)
+					is.remove(iii.i3);
+			}
+
+			// simplify remaining instructions
+
+			for (int p = 0; p < is.size(); p++) {
+				final IPair ii = getIPair(is, p);
+				if (ii.i1.d == ii.i2.d) { // Dx, Dy = Dx+y, D0
+					ii.i1.n += ii.i2.n;
+					ii.i2.n = 0;
+				} else if (OPPOS.contains(ii.toDs()))
+					if (ii.i1.n > ii.i2.n) { // Rn+m, Lm -> Rn, L0
+						ii.i1.n -= ii.i2.n;
+						this.res += ii.i2.n;
+						ii.i2.n = 0;
+					} else { // Rn, Ln+m -> R0, Ln
+						ii.i2.n -= ii.i1.n;
+						this.res += ii.i1.n;
+						ii.i1.n = 0;
+					}
+
+				// remove any set to 0
+				for (final I i : new I[] { ii.i1, ii.i2 })
+					if (i.n == 0) {
+						is.remove(i);
+						p--;
+					}
+			}
+
+			pp(is);
+			
+		}
+		
+		// area of remaining rect
+
+		this.res += (long) (is.get(0).n + 1) * (is.get(1).n + 1); // +1 for borders
 
 		return this;
 	}
 	
+	static class IPair {
+		I i1;
+		I i2;
+
+		String toDs() {
+			return "" + this.i1.d + this.i2.d;
+		}
+	}
+
+	static class IAround extends IPair {
+		I i3;
+		@Override
+		String toDs() {
+			return super.toDs() + this.i3.d;
+		}
+	}
+
+	private IPair getIPair(final List<I> is, final int p) {
+		final IPair ret = new IPair();
+		ret.i1 = is.get(p - 1 == -1 ? is.size() - 1 : p - 1);
+		ret.i2 = is.get(p);
+		return ret;
+	}
+
+	private IAround getIAround(final List<I> is, final int p) {
+		final IAround ret = new IAround();
+		ret.i1 = is.get(p - 1 == -1 ? is.size() - 1 : p - 1);
+		ret.i2 = is.get(p);
+		ret.i3 = is.get(p + 1 == is.size() ? 0 : p + 1);
+		return ret;
+	}
+
+	private boolean parseP2 = true;
+	
+	private Part2 setParseP1() {
+		this.parseP2 = false;
+		return this;
+	}
+
 	private boolean pp;
 
 	private Part2 setWithPP() {
@@ -116,6 +169,9 @@ public class Part2 extends Part1 {
 		return this;
 	}
 
+	private int xmin = 0, xmax = 0, ymin = 0, ymax = 0;
+	private boolean minmax = false;
+	
 	private void pp(final List<I> is) {
 		if (!this.pp)
 			return;
@@ -123,8 +179,6 @@ public class Part2 extends Part1 {
 		final Collection<P> path = new HashSet<>();
 		path.add(new P(0, 0));
 
-		int xmin = 0, xmax = 0, ymin = 0, ymax = 0;
-		
 		{
 			int x = 0, y = 0;
 
@@ -142,28 +196,25 @@ public class Part2 extends Part1 {
 					path.add(new P(x, y));
 				}
 				
-				xmin = Math.min(xmin, x);
-				xmax = Math.max(xmax, x);
-				ymin = Math.min(ymin, y);
-				ymax = Math.max(ymax, y);
+				if (!this.minmax) {
+					this.xmin = Math.min(this.xmin, x);
+					this.xmax = Math.max(this.xmax, x);
+					this.ymin = Math.min(this.ymin, y);
+					this.ymax = Math.max(this.ymax, y);
+				}
 			}
 		}
 
-		for (int y = ymin; y <= ymax; y++) {
+		this.minmax = true;
+
+		for (int y = this.ymin; y <= this.ymax; y++) {
 			String s = "";
-			for (int x = xmin; x <= xmax; x++) {
+			for (int x = this.xmin; x <= this.xmax; x++) {
 				final P p = new P(x, y);
 				s += x == 0 && y == 0 ? "+" : path.contains(p) ? "#" : ".";
 			}
 			System.out.println(s);
 		}
-	}
-
-	private boolean parseP2 = true;
-	
-	private Part2 setParseP1() {
-		this.parseP2 = false;
-		return this;
 	}
 
 	@Override
@@ -199,8 +250,9 @@ public class Part2 extends Part1 {
 
 	@Override
 	public void main() {
+		// assertEquals(52035, new Part2().setParseP1().setWithPP().compute("input.txt").res);
 		assertEquals(52035, new Part2().setParseP1().compute("input.txt").res);
-		// assertEquals(0, new Part2().compute("input.txt").res);
+		assertEquals(60612092439765l, new Part2().compute("input.txt").res);
 	}
 
 }
