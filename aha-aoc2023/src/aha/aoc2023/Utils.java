0a4076344bf201b8693a9cf3fa164bd4184c2a60
aha5811 <aha5811@users.noreply.github.com>
2023-12-21 10:07:25 +0100

    day 21 part 1

--- aha-aoc2023/src/aha/aoc2023/Utils.java
+++ aha-aoc2023/src/aha/aoc2023/Utils.java
@@ -8,13 +8,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Utils {
-
-	private static String dir = "/aha/aoc2023/";
 	
+	private static String dir = "/aha/aoc2023/";
+
 	public static Stream<String> streamLines(final String string) {
 		try {
 			return Files.lines(Paths.get(Utils.class.getResource(dir + string).toURI()));
@@ -23,27 +24,27 @@ public class Utils {
 			return null;
 		}
 	}
-	
+
 	public static List<String> readLines(final String file) {
 		return streamLines(file).collect(Collectors.toList());
 	}
-	
+
 	public static String reverse(final String s) {
 		return new StringBuilder(s).reverse().toString();
 	}
-	
+
 	private static Stream<String> toSs(final String s) {
 		return Arrays.stream(s.trim().split("\\s+"));
 	}
-	
+
 	public static List<Integer> toIs(final String s) {
 		return toSs(s).map(Integer::parseInt).collect(Collectors.toList());
 	}
-	
+
 	public static List<Long> toLs(final String s) {
 		return toSs(s).map(Long::parseLong).collect(Collectors.toList());
 	}
-
+	
 	public static <T> boolean same(final List<T> ts1, final List<T> ts2) {
 		if (ts1.size() != ts2.size())
 			return false;
@@ -53,26 +54,26 @@ public class Utils {
 				return false;
 		return true;
 	}
-
+	
 	/*
 	 * least common multiple
 	 */
 	public static long lcm(final long a, final long b) {
 		return a * b / gcd(a, b);
 	}
-
+	
 	/*
 	 * greatest common divisor
 	 */
 	public static long gcd(final long a, final long b) {
 		return b == 0 ? a : gcd(b, a % b);
 	}
-	
+
 	public static class CharMap {
 		public final int w;
 		public final int h;
 		public final char[][] chars;
-		
+
 		public CharMap(final List<String> lines) {
 			this.h = lines.size();
 			this.w = lines.get(0).length();
@@ -84,22 +85,22 @@ public class Utils {
 				y++;
 			}
 		}
-		
+
 		public CharMap(final String file) {
 			this(Utils.readLines(file));
 		}
-		
+
 		public Character getChar(final int x, final int y) {
 			if (x < 0 || y < 0 || x >= this.w || y >= this.h)
 				return null;
 			return this.chars[x][y];
 		}
-
+		
 		public Symbol getSymbol(final int x, final int y) {
 			final Character c = getChar(x, y);
 			return c == null ? null : new Symbol(c, x, y);
 		}
-
+		
 		@Override
 		public String toString() {
 			String ret = "";
@@ -111,7 +112,7 @@ public class Utils {
 			}
 			return ret;
 		}
-
+		
 		public List<Symbol> getAll(final char c) {
 			final List<Symbol> ret = new ArrayList<>();
 			for (int x = 0; x < this.w; x++)
@@ -122,24 +123,42 @@ public class Utils {
 				}
 			return ret;
 		}
-		
-	}
 
+	}
+	
 	public static class Symbol {
 		public final char c;
 		public final int x;
 		public final int y;
-
+		
 		public Symbol(final char c, final int x, final int y) {
 			this.c = c;
 			this.x = x;
 			this.y = y;
 		}
-		
+
 		@Override
 		public String toString() {
 			return this.x + "," + this.y + ":" + this.c;
 		}
+		
+		@Override
+		public int hashCode() {
+			return Objects.hash(this.c, this.x, this.y);
+		}
+		
+		@Override
+		public boolean equals(final Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			final Symbol other = (Symbol) obj;
+			return this.c == other.c && this.x == other.x && this.y == other.y;
+		}
+
 	}
-	
+
 }
